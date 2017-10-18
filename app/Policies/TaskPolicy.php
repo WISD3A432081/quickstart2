f63f71055be1760d0e61d85784353586f2cac476
chan\admin <s707071127@gmail.com>
2017-10-18 20:00:32 +0800

    增加一個 destroy 方法

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -3,7 +3,9 @@
 namespace App\Policies;
 
 use App\User;
+use App\Task;
 use Illuminate\Auth\Access\HandlesAuthorization;
+
 
 class TaskPolicy
 {
@@ -14,8 +16,8 @@ class TaskPolicy
      *
      * @return void
      */
-    public function __construct()
+    public function destroy(User $user, Task $task)
     {
-        //
+        return $user->id === $task->user_id;
     }
 }
