644fd5c084cbacb76c48d125b7014dd64ad75264
chan\admin <s707071127@gmail.com>
2017-10-18 18:35:20 +0800

    user修改

--- app/User.php
+++ app/User.php
@@ -1,13 +1,26 @@
 <?php
-
 namespace App;
-
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
 class User extends Authenticatable
 {
-    // 其他的 Eloquent 屬性...
-
+    use Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
     /**
      * 取得該使用者的所有任務。
      */
